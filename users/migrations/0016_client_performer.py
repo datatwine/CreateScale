def154aef4a9d3946636712eefb9b2d6ba2c729f
Rajath90 <rajathgowda472@gmail.com>
2026-08-22 14:56:03 +0530

    chore: fix Profile/ClientAdmin imports and format

--- users/migrations/0016_client_performer.py
+++ users/migrations/0016_client_performer.py
@@ -1,36 +1,34 @@
 from django.db import migrations
 
-class Migration(migrations.Migration):
 
+class Migration(migrations.Migration):
     dependencies = [
-        ('users', '0015_pushtoken'),
+        ("users", "0015_pushtoken"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Client',
-            fields=[
-            ],
+            name="Client",
+            fields=[],
             options={
-                'verbose_name': 'Client',
-                'verbose_name_plural': 'Clients',
-                'proxy': True,
-                'indexes': [],
-                'constraints': [],
+                "verbose_name": "Client",
+                "verbose_name_plural": "Clients",
+                "proxy": True,
+                "indexes": [],
+                "constraints": [],
             },
-            bases=('users.profile',),
+            bases=("users.profile",),
         ),
         migrations.CreateModel(
-            name='Performer',
-            fields=[
-            ],
+            name="Performer",
+            fields=[],
             options={
-                'verbose_name': 'Performer',
-                'verbose_name_plural': 'Performers',
-                'proxy': True,
-                'indexes': [],
-                'constraints': [],
+                "verbose_name": "Performer",
+                "verbose_name_plural": "Performers",
+                "proxy": True,
+                "indexes": [],
+                "constraints": [],
             },
-            bases=('users.profile',),
+            bases=("users.profile",),
         ),
     ]
